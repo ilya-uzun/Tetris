6f9131777627944fa120e94340304c84b25dbcbe
ilya-uzun <ilya.uzun@bk.ru>
2021-06-27 17:41:16 +0500

    отображаются элементы дочернего Layout

--- app/src/main/java/com/example/tetris/MainActivity.kt
+++ app/src/main/java/com/example/tetris/MainActivity.kt
@@ -2,11 +2,12 @@ package com.example.tetris
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val linearLayout: LinearLayout = LinearLayout(this) 
+        val linearLayout: LinearLayout = LinearLayout(this)
     }
 }
