6931cfd59df47ce02189b979f537ab3f137d684a
ilya-uzun <ilya.uzun@bk.ru>
2021-06-29 20:50:18 +0500

    событие по кнопке

--- app/src/main/java/com/example/tetris/MainActivity.kt
+++ app/src/main/java/com/example/tetris/MainActivity.kt
@@ -3,11 +3,26 @@ package com.example.tetris
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.LinearLayout
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.ActionBar
 
 class MainActivity : AppCompatActivity() {
+
+    var tvHighScore: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val linearLayout: LinearLayout = LinearLayout(this)
+        supportActionBar?.hide() // если ссылка не является пустой то объект вызывает метод hide
+        val btnNewGame = findViewById<Button>(R.id.btn_new_game)
+        val btnResetScore = findViewById<Button>(R.id.btn_reset_score)
+        val btnExit = findViewById<Button>(R.id.btn_exit)
+    }
+
+    fun handleExitEvent(view: View){
+        finish()
     }
 }
