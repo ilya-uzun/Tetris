825992cf16f4e8ce832abaacd143ceeb51614f20
ilya-uzun <ilya.uzun@bk.ru>
2021-07-07 13:40:07 +0500

    class Block.java

---
+++ app/src/main/java/com/example/tetris/models/Block.java
@@ -0,0 +1,23 @@
+package com.example.tetris.models;
+import android.graphics.Color;
+import android.graphics.Point;
+
+public class Block {
+    private int shapeIndex;
+    private int framaNumber;
+    private BlockColor color;
+    private Point position;
+    public  emun BlockColor{
+        PINK(Color.rgb(255,105,180), (byte) 2),
+        GREEN(Color.rgb(0,128,0), (byte) 3),
+        ORANGE(Color.rgb(255,140,0), (byte) 4),
+        YELLOW(Color.rgb(255,140,0), (byte) 5),
+        CYAN(Color.rgb(0,255,255), (byte) 6);
+        BlockColor(int rgbValue, byte value) {
+            this.rgbValue = rgbValue;
+            this.byteValue = byteValue;
+        }
+        private final int rgbValue;
+        private final int byteValue;
+    }
+}
